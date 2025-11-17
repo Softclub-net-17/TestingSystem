0e28cea421af17a3be9685ffee9724383177c475
MehronaA <asoevamehrona9@gmail.com>
2025-11-17 15:36:44 +0500

    Imlemented auth and user endpoints

--- Application/Auth/Validators/RegisterValidator.cs
+++ Application/Auth/Validators/RegisterValidator.cs
@@ -25,9 +25,6 @@ public class RegisterValidator : IValidator<RegisterCommand>
         if (!instance.Email.Contains('@'))
             validationResult.AddError("Email is not in correct format.");
 
-        if (instance.Email.Trim().Length < 20)
-            validationResult.AddError("Too short email");
-
         if (instance.Email.Trim().Length > 200)
             validationResult.AddError("Too long email");
 
